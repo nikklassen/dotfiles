6d52a4382c12cb0e17a6bb9119f41933aec63342
Nik Klassen <nikklassen@users.noreply.github.com>
2024-04-13 11:12:36 -0400

    Remove wezterm config window_padding

--- wezterm-base.lua
+++ wezterm-base.lua
@@ -10,6 +10,13 @@ config.warn_about_missing_glyphs = false
 
 config.audible_bell = 'Disabled'
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 -- Use the defaults as a base
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
