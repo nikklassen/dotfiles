52b5c7acaa74fbad0d1d1b1af612ef20c3afed7c
Nik Klassen <nikklassen@users.noreply.github.com>
2025-01-11 09:24:54 -0500

    Move blink to its own file

---
+++ vim/lua/nikklassen/plugins/blink.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    'saghen/blink.compat',
+    version = '*',
+    lazy = true,
+    opts = {
+      impersonate_nvim_cmp = true,
+      debug = true,
+    },
+  },
+  {
+    'saghen/blink.cmp',
+    lazy = false, -- lazy loading handled internally
+
+    version = 'v0.*',
+    -- build = 'cargo build --release',
+
+    opts = {
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      appearance = {
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release, assuming themes add support
+        use_nvim_cmp_as_default = true,
+      },
+
+      completion = {
+        list = {
+          selection = {
+            preselect = function(_) return not require('blink.cmp').snippet_active({ direction = 1 }) end
+          }
+        },
+
+        menu = {
+          auto_show = true,
+        },
+
+        accept = {
+          auto_brackets = {
+            enabled = true,
+            -- blocked_filetypes = { 'kotlin' },
+          },
+        },
+      },
+
+      keymap = {
+        preset = 'super-tab',
+        ['<C-y>'] = { 'select_and_accept' },
+      },
+
+      signature = {
+        enabled = true,
+      },
+    },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" }
+  },
+}
