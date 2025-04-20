3dcc556675bd78b8b7d0db51fd2a98107b1dda27
Nik Klassen <nikklassen@users.noreply.github.com>
2025-04-20 13:29:22 -0400

    Prefer sort text for blink.cmp sorting

--- vim/lua/nikklassen/plugins/blink.lua
+++ vim/lua/nikklassen/plugins/blink.lua
@@ -58,6 +58,14 @@ return {
           ['<C-e>'] = { 'cancel', 'fallback' },
         },
       },
+
+      fuzzy = {
+        sorts = {
+          -- Blink default is reversed, sort_text seems better for Go. May need to customize this per language
+          'sort_text',
+          'score',
+        },
+      },
     },
     -- allows extending the providers array elsewhere in your config
     -- without having to redefine it
