c8c3cf2cb60111dfc1a89940d814bfbcaa0a47c6
Nik Klassen <nikklassen@users.noreply.github.com>
2025-08-23 14:04:26 -0400

    Use explicit path for chezmoi in install script

--- install/install.zsh
+++ install/install.zsh
@@ -3,4 +3,4 @@
 if ! command chezmoi > /dev/null 2>&1; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
 fi
-chezmoi init --apply --source "${PWD?}" nikklassen
+~/.local/bin/chezmoi init --apply --source "${PWD?}" nikklassen
