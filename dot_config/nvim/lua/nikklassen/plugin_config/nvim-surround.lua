f0499daa991bba37c71c2e9d59aa505321034cb5
Nik Klassen <nikklassen@users.noreply.github.com>
2025-10-16 13:28:17 -0400

    Update custom type surround for changes on main branch

--- dot_config/nvim/lua/nikklassen/plugin_config/nvim-surround.lua
+++ dot_config/nvim/lua/nikklassen/plugin_config/nvim-surround.lua
@@ -14,15 +14,17 @@ return {
         end
       end,
       delete = function()
-        local shared = require('nvim-treesitter.textobjects.shared')
-        local _, outer_textobject = shared.textobject_at_point('@type.outer', 'textobjects', nil, nil,
+        local shared = require('nvim-treesitter-textobjects.shared')
+        local outer_textobject = shared.textobject_at_point('@type.outer', 'textobjects', nil, nil,
           { lookahead = false, lookbehind = false })
         if outer_textobject == nil then
+          vim.notify('Could not find outer type', vim.log.levels.ERROR)
           return nil
         end
-        local _, inner_textobject = shared.textobject_at_point('@type.inner', 'textobjects', nil, nil,
+        local inner_textobject = shared.textobject_at_point('@type.inner', 'textobjects', nil, nil,
           { lookahead = false, lookbehind = false })
         if inner_textobject == nil then
+          vim.notify('Could not find inner type', vim.log.levels.ERROR)
           return nil
         end
         return {
@@ -31,8 +33,8 @@ return {
             last_pos = { inner_textobject[1] + 1, inner_textobject[2] + 1 },
           },
           right = {
-            first_pos = { inner_textobject[3] + 1, inner_textobject[4] },
-            last_pos = { outer_textobject[3] + 1, outer_textobject[4] },
+            first_pos = { inner_textobject[4] + 1, inner_textobject[5] },
+            last_pos = { outer_textobject[4] + 1, outer_textobject[5] },
           },
         }
       end,
