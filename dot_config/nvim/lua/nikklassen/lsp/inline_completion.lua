db4df1c958d8e7343a2df9e9c07ff4dcafbbe5bd
Nik Klassen <nikklassen@users.noreply.github.com>
2025-11-01 14:27:40 -0400

    Fix inline completion jumping all windows for the buffer

--- dot_config/nvim/lua/nikklassen/lsp/inline_completion.lua
+++ dot_config/nvim/lua/nikklassen/lsp/inline_completion.lua
@@ -2,80 +2,69 @@ local ms = vim.lsp.protocol.Methods
 
 local M = {}
 
-local function accept_completion(item, mode)
+local function accept_string_completion(item, mode, insert_text, win_id)
+  local range = item.range
+  if not range then
+    vim.api.nvim_paste(insert_text, false, 0)
+    return
+  end
+
+  local lines = vim.split(insert_text, '\n')
+  local current_lines = vim.api.nvim_buf_get_text(range.start.buf, range.start.row, range.start.col, range.end_.row,
+    range.end_.col, {})
+
+  if mode == 'word' then
+    local row = 1
+    while row <= #lines and row <= #current_lines and lines[row] == current_lines[row] do
+      row = row + 1
+    end
+
+    local col = 1
+    while
+      row <= #lines
+      and col <= #lines[row]
+      and row <= #current_lines
+      and col <= #current_lines[row]
+      and lines[row][col] == current_lines[row][col]
+    do
+      col = col + 1
+    end
+
+    local word = string.match(lines[row]:sub(col), '%s*[^%s]%w*')
+    vim.api.nvim_buf_set_text(
+      range.start.buf,
+      math.min(range.start.row + row - 1, range.end_.row),
+      row <= #current_lines and (row == 1 and range.start.col + col - 1 or col - 1) or range.end_.col,
+      range.end_.row,
+      range.end_.col,
+      row <= #current_lines and { word } or { '', word }
+    )
+    local pos = item.range.start:to_cursor()
+    vim.api.nvim_win_set_cursor(win_id, {
+      pos[1] + row - 1,
+      pos[2] + col - 1 + #lines[1] - 1,
+    })
+  else
+    vim.api.nvim_buf_set_text(
+      range.start.buf,
+      range.start.row,
+      range.start.col,
+      range.end_.row,
+      range.end_.col,
+      lines
+    )
+    local pos = item.range.start:to_cursor()
+    vim.api.nvim_win_set_cursor(win_id, {
+      pos[1] + #lines - 1,
+      (#lines == 1 and pos[2] or 0) + #lines[#lines],
+    })
+  end
+end
+
+local function accept_completion(item, mode, win_id)
   local insert_text = item.insert_text
   if type(insert_text) == 'string' then
-    local range = item.range
-    if range then
-      local lines = vim.split(insert_text, '\n')
-      local current_lines = vim.api.nvim_buf_get_text(
-        range.start.buf,
-        range.start.row,
-        range.start.col,
-        range.end_.row,
-        range.end_.col,
-        {}
-      )
-
-      if mode == 'word' then
-        local row = 1
-        while row <= #lines and row <= #current_lines and lines[row] == current_lines[row] do
-          row = row + 1
-        end
-
-        local col = 1
-        while
-          row <= #lines
-          and col <= #lines[row]
-          and row <= #current_lines
-          and col <= #current_lines[row]
-          and lines[row][col] == current_lines[row][col]
-        do
-          col = col + 1
-        end
-
-        local word = string.match(lines[row]:sub(col), '%s*[^%s]%w*')
-        -- vim.print(
-        --   range.start.buf,
-        --   math.min(range.start.row + row - 1, range.end_.row),
-        --   row == #current_lines and math.min(range.start.col + col - 1, range.end_.col)
-        --   or math.min(col - 1, range.end_.col),
-        --   range.end_.row,
-        --   range.end_.col,
-        --   { word }
-        -- )
-
-        vim.api.nvim_buf_set_text(
-          range.start.buf,
-          math.min(range.start.row + row - 1, range.end_.row),
-          row <= #current_lines and (row == 1 and range.start.col + col - 1 or col - 1) or range.end_.col,
-          range.end_.row,
-          range.end_.col,
-          row <= #current_lines and { word } or { '', word }
-        )
-        local pos = item.range.start:to_cursor()
-        vim.api.nvim_win_set_cursor(vim.fn.bufwinid(range.start.buf), {
-          pos[1] + row - 1,
-          pos[2] + col - 1 + #lines[1] - 1,
-        })
-      else
-        vim.api.nvim_buf_set_text(
-          range.start.buf,
-          range.start.row,
-          range.start.col,
-          range.end_.row,
-          range.end_.col,
-          lines
-        )
-        local pos = item.range.start:to_cursor()
-        vim.api.nvim_win_set_cursor(vim.fn.bufwinid(range.start.buf), {
-          pos[1] + #lines - 1,
-          (#lines == 1 and pos[2] or 0) + #lines[#lines],
-        })
-      end
-    else
-      vim.api.nvim_paste(insert_text, false, 0)
-    end
+    accept_string_completion(item, mode, insert_text, win_id)
   elseif insert_text.kind == 'snippet' then
     vim.snippet.expand(insert_text.value)
   end
@@ -99,9 +88,12 @@ function M.attach(client, bufnr)
     bufnr = bufnr,
   })
   vim.keymap.set('i', '<C-CR>', function()
+    local win_id = vim.api.nvim_get_current_win()
     if not vim.lsp.inline_completion.get({
           bufnr = bufnr,
-          on_accept = accept_completion,
+          on_accept = function(item)
+            accept_completion(item, nil, win_id)
+          end
         }) then
       return '<C-CR>'
     end
@@ -112,10 +104,11 @@ function M.attach(client, bufnr)
     buffer = bufnr,
   })
   vim.keymap.set('i', '<C-Right>', function()
+    local win_id = vim.api.nvim_get_current_win()
     if not vim.lsp.inline_completion.get({
           bufnr = bufnr,
           on_accept = function(item)
-            accept_completion(item, 'word')
+            accept_completion(item, 'word', win_id)
           end,
         }) then
       return '<C-Right>'
