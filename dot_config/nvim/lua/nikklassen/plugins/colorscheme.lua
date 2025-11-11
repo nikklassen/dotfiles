333f021e349d8c921908e76150dcf021bb80e15d
Nik Klassen <nikklassen@users.noreply.github.com>
2025-11-11 05:47:31 -0500

    Clean up colorscheme

--- dot_config/nvim/lua/nikklassen/plugins/colorscheme.lua
+++ dot_config/nvim/lua/nikklassen/plugins/colorscheme.lua
@@ -7,12 +7,14 @@ return {
       local onedark = require 'onedark'
       onedark.setup()
       onedark.load()
-      vim.cmd('hi! link @lsp.type.variable @variable')
-      vim.cmd('hi! link @lsp.type.property @field')
-      vim.cmd('hi! link @lsp.type.namespace @variable')
-      vim.cmd('hi! link @lsp.typemod.variable.defaultLibrary @constant.builtin')
-      vim.cmd('hi! link @lsp.typemod.enumMember.defaultLibrary @constant.builtin')
-      vim.cmd('hi clear @spell')
+      vim.cmd([[
+        hi! link @lsp.type.variable @variable
+        hi! link @lsp.type.property @field
+        hi! link @lsp.type.namespace @variable
+        hi! link @lsp.typemod.variable.defaultLibrary @constant.builtin
+        hi! link @lsp.typemod.enumMember.defaultLibrary @constant.builtin
+        hi clear @spell
+      ]])
     end,
   },
 }
