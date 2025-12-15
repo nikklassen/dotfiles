a5904b37e4dde910dfe03286074f1c3a97ffcedf
Nik Klassen <nikklassen@users.noreply.github.com>
2025-12-15 09:53:58 -0500

    Fix go treesitter highlighting getting overriden by lower priority rules

--- dot_config/nvim/lua/nikklassen/plugins/colorscheme.lua
+++ dot_config/nvim/lua/nikklassen/plugins/colorscheme.lua
@@ -9,6 +9,7 @@ return {
       onedark.load()
       vim.cmd([[
         hi! link @lsp.type.variable @variable
+        hi! link @lsp.type.variable.go @variable
         hi! link @lsp.type.property @field
         hi! link @lsp.typemod.variable.defaultLibrary @constant.builtin
         hi! link @lsp.typemod.enumMember.defaultLibrary @constant.builtin
