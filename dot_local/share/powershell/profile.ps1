c0c07cdf9d1f781e9f5f68038984323195770fda
Nik Klassen <nikklassen@users.noreply.github.com>
2026-04-23 17:33:14 -0400

    Windows updates

--- dot_local/share/powershell/profile.ps1
+++ dot_local/share/powershell/profile.ps1
@@ -9,8 +9,12 @@ Import-Module PSFzf
 function ga { git add @args }
 function gd { git diff @args }
 function gdca { git diff --cached @args }
-function gl { git log @args }
+function gl { git pull @args }
 function gst { git status @args }
+function glog { git log --oneline --decorate --graph }
+function hx { glog }
+
+function cma { chezmoi apply }
 
 Set-Alias which Get-Command
 
@@ -19,3 +23,5 @@ $env:FZF_CTRL_T_COMMAND="$env:FZF_DEFAULT_COMMAND"
 $env:FZF_DEFAULT_OPTS="--keep-right --bind=home:first,end:last"
 
 $env:TERM='xterm-256color'
+
+cd ~\.local\share\chezmoi
